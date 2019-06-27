a239ff9e65d0558de19b0eed181982f8b009332a
ZijieZh <50355277+ZijieZh@users.noreply.github.com>
2019-06-27 15:37:03 +0800

    Add files via upload

---
+++ 1144correct.cpp
@@ -0,0 +1,16 @@
+#include <map>
+#include<iostream>
+using namespace std;
+int main() {
+	int n, a, num = 0;
+	cin >> n;
+	map<int, int> m;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		m[a]++;
+	}
+	while (++num)
+		if (m[num] == 0) break;
+	cout << num;
+	return 0;
+}
